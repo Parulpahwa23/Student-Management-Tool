d07df8728a217154acac1199610ffe520365731f
parul <parulpahwa2021@gmail.com>
2026-07-02 23:49:43 +0530

    Fix Tailwind v4 configuration and mongoose connection logic

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 
 // Database Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
